d86384fffd1c8e21e20546b19220defde9f03f77
Will Smithee <wasmithee@gmail.com>
2022-09-10 12:00:51 -0600

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+import { defineNuxtConfig } from 'nuxt'
+import { fileURLToPath } from 'url'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  alias: {
+    'content': fileURLToPath(new URL('./functions', import.meta.url))
+  },
+  css: ['~/assets/css/myfile.less'],
+  components: [
+    '~/components',
+  ],
+  content: [
+    '~/content',
+    '~/functions',
+  ],
+  plugins: ['~/plugins/ckeditor-nuxt'],
+  modules: ['@nuxt/content', '@nuxt/ui', '@nuxtjs/tailwindcss']
+  // modules: ['@nuxt/content', '@nuxt/ui']
+})
