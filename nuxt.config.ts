1a0cb623b0520cc60c7e2efb21551d8705cda97f
Will Smithee <wasmithee@users.noreply.github.com>
2022-10-10 14:24:28 -0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ import { fileURLToPath } from 'url'
 
 export default defineNuxtConfig({
   publicRuntimeConfig: {
-    BASE_DOMAIN: process.env.BASE_DOMAIN
+    BASE_DOMAIN: process.env.BASE_DOMAIN,
+    WORKERS_ARRAY: process.env.WORKERS_ARRAY
   },
   alias: {
     'content': fileURLToPath(new URL('./functions', import.meta.url))
